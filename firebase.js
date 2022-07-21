5857695527440d3be42f125a8d9a59aaea72199b
UgbolueWilliams <ugboluewilliams@yahoo.com>
2022-07-21 08:24:34 +0100

    working on the project

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8fNvPjXoQ4kTZ5i7cmhP2j-EINf0WSGw",
+  authDomain: "challenge-b4122.firebaseapp.com",
+  projectId: "challenge-b4122",
+  storageBucket: "challenge-b4122.appspot.com",
+  messagingSenderId: "745742678406",
+  appId: "1:745742678406:web:19018e7804a41d4a75d407",
+  measurementId: "G-4ZX27SDTK4",
+};
